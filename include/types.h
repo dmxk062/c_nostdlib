75f569c8f7aecf8ec5745459c918a1a870a0b72d
dmxk062 <mxk06@protonmail.com>
2024-04-20 09:30:05 +0200

    start working on a result type

--- include/types.h
+++ include/types.h
@@ -1,6 +1,8 @@
 // vim: ft=c
 #pragma once
 
+#define NULL        0
+
 // no use for them right now
 // typedef signed __int128     i128;
 // typedef unsigned __int128   u128;
@@ -14,16 +16,25 @@
  *      - f : float
  */
 
+
+// 64 bit signed integer
 typedef signed long int     i64;
+// 64 bit unsigned integer
 typedef unsigned long int   u64;
 
+// 32 bit signed integer
 typedef signed int          i32;
+// 32 bit unsigned integer
 typedef unsigned int        u32;
 
+// 16 bit signed integer
 typedef signed short        i16;
+// 32 bit unsigned integer
 typedef unsigned short      u16;
 
+// 8 bit signed integer
 typedef signed char         i8;
+// 8 bit unsigned integer
 typedef unsigned char       u8;
 
 
@@ -31,14 +42,24 @@ typedef long double         f128;
 typedef double              f64;
 typedef float               f32;
 
+typedef _Bool               bool;
+#define TRUE                1
+#define FALSE               0
+
 /*
- * Functions should generally return either i64 or bool to indicate success
- * bool if there only are two states and no additional information would be useful
- * i64  if there is some additional information (= error code) the function wants to communicate
+ * Return value for a function
+ * Check for errors before using value or errno
  */
-typedef u8                  bool;
+typedef struct {
+    bool        success;
+    union {
+        void*   value;
+        u64     errno;
+    };
+} result;
 
 
-#define NULL        0
-#define TRUE        1
-#define FALSE       0
+// zero terminated string
+
+typedef char*               zstr;
+
