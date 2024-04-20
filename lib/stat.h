1f87f679d34b526357f2da5a8a5fd541b49fb498
dmxk062 <mxk06@protonmail.com>
2024-04-20 22:34:18 +0200

    fanciest stat struct layout ever, eliminiate need for binary operators

--- lib/stat.h
+++ lib/stat.h
@@ -8,34 +8,36 @@
 
 
 
+union struct_mode {
+    u32 st_mode_bits;
+    union {
+        union {
+            u16 perms : 12;
+            struct {
 
-struct stat_mode {
-    u8 : 3; // padd
+                bool    oread  : 1;
+                bool    owrite : 1;
+                bool    oexec  : 1;
 
-    bool    isreg  : 1;
-    bool    isdir  : 1;
-    bool    ischr  : 1;
-    bool    isblk  : 1;
-    bool    isfifo : 1;
-    bool    issock : 1;
+                bool    gread  : 1;
+                bool    gwrite : 1;
+                bool    gexec  : 1;
 
+                bool    uread  : 1;
+                bool    uwrite : 1;
+                bool    uexec  : 1;
 
-    u8 : 2; // padd
-    bool    suid : 1;
-    bool    sgid : 1;
-    bool    stck : 1;
+                bool    suid   : 1;
+                bool    sgid   : 1;
+                bool    sticky : 1;
 
-    bool    uread  : 1;
-    bool    uwrite : 1;
-    bool    uexec  : 1;
-
-    bool    gread  : 1;
-    bool    gwrite : 1;
-    bool    gexec  : 1;
-
-    bool    oread  : 1;
-    bool    owrite : 1;
-    bool    oexec  : 1;
+            };
+        };
+        struct {
+            u32 : 12;
+            u8  type : 4;
+        };
+    };
 
 };
 
@@ -44,10 +46,7 @@ struct stat {
     u64     st_ino;
     u64     st_nlink;
 
-    union {
-        u32              st_mode_num;
-        struct stat_mode st_mode;
-    };
+    union struct_mode mode;
     u32     st_uid;
     u32     st_gid;
     u32     : 4;
@@ -73,3 +72,11 @@ struct stat {
 i64 stat(char* path, struct stat* statbuf);
 i64 lstat(char* path, struct stat* statbuf);
 i64 fstat(u64 fd, struct stat* statbuf);
+
+#define	S_DIR     004
+#define	S_CHR     002
+#define	S_BLK     006
+#define	S_REG     010
+#define	S_IFO     001
+#define	S_LNK     012
+#define	S_SOCK    014
