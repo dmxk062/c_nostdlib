8f7d29c6854a60726d2b1ef716f7cc39d049f1d1
dmxk062 <mxk06@protonmail.com>
2024-04-28 00:10:50 +0200

    add directory iterator

--- lib/dirent.c
+++ lib/dirent.c
@@ -1,4 +1,45 @@
 #include "include/dirent.h"
 #include "include/io.h"
+#include "include/types.h"
+#include "include/syscall.h"
 
+RESULT(u64) opendir(const char* path) {
+    u64 mode = O_RDONLY|O_DIRECTORY|O_CLOEXEC;
+    return open(path, NULL, mode);
+}
 
+RESULT(u64) _getdents(u64 fd, dirent* ent, u64 count) {
+    i64 ret = (i64) syscall3(SYS_GETDENTS64, 
+            (untyped)fd,
+            ent,
+            (untyped)count);
+    if (ret < 0)
+        return (RESULT(u64)){.success = FALSE, .errno = -ret};
+    else
+        return (RESULT(u64)){.success = TRUE, .value = ret};
+}
+
+RESULT(dirent) nextdir(u64 fd, directory_buffer* buf) {
+    bool new_entries = FALSE;
+    while (TRUE) {
+        if (buf->len == 0 || buf->len < buf->offset || new_entries) {
+            RESULT(u64) ret;
+            ret = _getdents(fd, buf->buffer, DIRENT_BUF_SIZE);
+            if (!ret.success || ret.value == 0) {
+                return (RESULT(dirent)){.success = FALSE, .errno = ret.errno};
+            }
+            buf->len = ret.value;
+            buf->offset = 0;
+            new_entries = FALSE;
+        }
+
+        dirent* d;
+        while (buf->offset < buf->len) {
+            d = (dirent*)(buf->buffer + buf->offset);
+            buf->offset += d->len;
+            if (d->ino != 0)
+                return (RESULT(dirent)){.success = TRUE, .value = d};
+        }
+        new_entries = TRUE;
+    }
+}
--- lib/include/dirent.h
+++ lib/include/dirent.h
@@ -1,13 +1,37 @@
 #pragma once
 #include "types.h"
 
-struct dirent {
-    u64     inode;
-    i64     offset;
+#define DIRENT_BUF_SIZE 256
 
-    u16     reclen;
+typedef struct dirent {
+    u64     ino;
+    u64     off;
+
+    u16     len;
     u8      type;
-    char    name[256];
-};
+    char    name[5];
+} dirent;
 
-u64 opendir(const char* path);
+typedef struct directory_buffer {
+    u64     offset;
+    u64     len;
+    char    buffer[DIRENT_BUF_SIZE];
+} directory_buffer;
+
+RESULT(u64) opendir(const char* path);
+
+RESULT(u64) _getdents(u64 fd, dirent* ent, u64 count);
+
+DEFRESULT(dirent*, dirent);
+RESULT(dirent) nextdir(u64 fd, directory_buffer* buf);
+
+#define DT_UNKNOWN  0
+#define DT_FIFO     1
+#define DT_CHR      2
+#define DT_DIR      4
+#define DT_BLK      6
+#define DT_REG      8
+#define DT_LNK      10
+#define DT_SOCK     12
+#define DT_WHT      14
+
