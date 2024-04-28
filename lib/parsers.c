2809fb2d51be5aad79e197845e3a11b59713fb64
dmxk062 <mxk06@protonmail.com>
2024-04-28 11:32:09 +0200

    clean up parser naming a bit

--- lib/parsers.c
+++ lib/parsers.c
@@ -16,7 +16,7 @@ inline i64 char_to_int(char c) {
 /*
  * parse a string into an integer, zero error tolerance
  */
-RESULT(i64) str_to_int(const char* string, u64 n, u64 base) {
+RESULT(i64) str_to_int(const char* string, u64 lenth, u64 base) {
     i64 parsed = 0;
 
     bool is_negative = FALSE;
@@ -24,10 +24,10 @@ RESULT(i64) str_to_int(const char* string, u64 n, u64 base) {
     if (string[0] == '-') {
         is_negative = TRUE;
         string++;
-        n--;
+        lenth--;
     }
 
-    for (i64 i = 0; i < n; i++) {
+    for (i64 i = 0; i < lenth; i++) {
         i64 digit = char_to_int(string[i]);
         // we got smth we can't really handle, tell the caller at which index
         if (digit == -1 || digit >= base) {
@@ -35,7 +35,7 @@ RESULT(i64) str_to_int(const char* string, u64 n, u64 base) {
         }
         // calculate the positional value
         i64 pwr = 1;
-        for (i64 j = 0; j < n - 1 -i; j++) {
+        for (i64 j = 0; j < lenth - 1 -i; j++) {
             pwr *= base;
         }
         parsed += digit * pwr;
@@ -53,7 +53,7 @@ RESULT(i64) str_to_int(const char* string, u64 n, u64 base) {
  * accepted format:
  * (-)[0-9].[0-9]
  */
-RESULT(f128) str_to_float(const char* string, u64 n) {
+RESULT(f128) str_to_float(const char* string, u64 length) {
     f128 result;
 
     bool is_negative;
@@ -66,15 +66,17 @@ RESULT(f128) str_to_float(const char* string, u64 n) {
     if (string[0] == '-') {
         is_negative = TRUE;
         string++;
-        n--;
+        length--;
     }
 
-    // figure out where/if the decimal point is
-    // TODO: add support for locale specific ones like ','
-    i64 decimal_position = strfind(string, n, '.');
+    /* 
+     * figure out where/if the decimal point is
+     * TODO: add support for locale specific ones like ','
+    */
+    i64 decimal_position = strfind(string, length, '.');
     if (decimal_position < 0) {
         has_decimals = FALSE;
-        target = n;
+        target = length;
     }  else {
         has_decimals = TRUE;
         target = decimal_position;
@@ -94,15 +96,17 @@ RESULT(f128) str_to_float(const char* string, u64 n) {
         integer += digit * pwr;
     }
 
-    // we don't even have to care about the component < 0
-    // just return the first part
+    /*
+     * we don't even have to care about the component < 0
+     * just return the first part
+     */
     if (!has_decimals) {
         if (is_negative) integer = -integer;
         return (RESULT(f128)){.success = TRUE, .value = integer};
     }
 
     // iterate over the rest of the string
-    for (i64 i = decimal_position + 1; i < n; i++) {
+    for (i64 i = decimal_position + 1; i < length; i++) {
         i64 digit = char_to_int(string[i]);
         if (digit == -1 || digit >= 10) {
             return (RESULT(f128)){.success = FALSE, .errno = i};
