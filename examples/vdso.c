6bd3f2324c348e5f074b397e48b20f39153572e5
dmxk062 <mxk06@protonmail.com>
2024-07-26 22:12:02 +0200

    examples: update vdso example

--- examples/vdso.c
+++ examples/vdso.c
@@ -1,25 +1,24 @@
-#include "io.h"
-#include "time.h"
-#include "types.h"
+#include "format.h"
+#include <io.h>
+#include <types.h>
 #include <linux/auxv.h>
 #include <linux/vdso.h>
 
-#define SYMBOL "__vdso_gettimeofday"
+#define SYMBOL "__vdso_getcpu"
 
 i32 main(i32 argc, zstr argv[], zstr envv[]) {
     Elf64Auxval* auxv = Auxval_find_auxv(envv);
 
     address vdsoaddr = Auxval_get_value(auxv, AuxvalType_VDSO_ADDRESS)->value;
 
-    i32 (*gettimeofday)(struct Timespec* t, struct Timespec* z) = VDSO_find_symbol(vdsoaddr, SYMBOL);
-    if (!gettimeofday) {
-        print("Failed to find "SYMBOL" in the vDSO\n");
+    i32 (*getcpu)(u32* cpu, u32* node) = VDSO_find_symbol(vdsoaddr, SYMBOL);
+    if (!getcpu) {
+        print("Failed to get "SYMBOL" from vDSO, aborting\n");
         return 1;
     }
 
-
-    struct Timespec sp = {0};
-    gettimeofday(&sp, NULL);
-    fprint(SYMBOL"()={ secs = %d, nano = %d}\n", (fmts){{.i = sp.secs}, {.i = sp.nano}});
+    u32 cpu = 0, node = 0;
+    getcpu(&cpu, &node);
+    fprint("Running on cpu: %d; NUMA node: %d\n", Fmt({.i = cpu}, {.i = node}));
     return 0;
 }
