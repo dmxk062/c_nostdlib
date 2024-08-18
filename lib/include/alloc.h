f4b8b232bf92c87a10b4f1205e5d040dfd87a232
dmxk062 <mxk06@protonmail.com>
2024-08-18 13:03:46 +0200

    start working on new allocator

--- lib/include/alloc.h
+++ lib/include/alloc.h
@@ -1,28 +1,8 @@
 #pragma once
 #ifndef _ALLOC_H
 #define _ALLOC_H
-
 #include <types.h>
 
-typedef struct mem_chunk {
-    u64 size;
-    bool free;
 
-    struct mem_chunk* next;
-    struct mem_chunk* prev;
-
-    char end[1];
-} MallocChunk;
-
-#define MEM_STRUCT_SIZE sizeof(MallocChunk)
-
-#define MEM_PAGE_SIZE 1024 * 2
-
-void*   malloc(u64 size);
-void free(void* ptr);
-
-u64 Malloc_get_used_count(void);
-u64 Malloc_get_page_count(void);
-u64 Malloc_get_allocated_count(void);
 
 #endif
